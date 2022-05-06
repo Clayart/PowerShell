9d60b5c2b1463620dc230ac9ad640e29b550aa0e
Clayton Gilchrist <56773065+Clayart@users.noreply.github.com>
2022-05-06 18:53:39 -0400

    Create getScheduledTaskUserId.ps1

---
+++ getScheduledTaskUserId.ps1
@@ -0,0 +1,14 @@
+#This script will list all scheduled tasks and their corresponding UserID, Status, and LogonType (and TaskPath if needed).
+#This is especially useful for quickly looking through scheduled tasks to see if any are using user credentials to run.
+
+$tasks = Get-ScheduledTask -TaskName *
+$results = foreach ($task in $tasks) {
+    [PSCustomObject]@{
+        TaskName = $task.TaskName
+        UserId   = $task.Principal.UserId
+        Status = $task.State
+        LogonType = $task.Principal.LogonType
+        #TaskPath = $task.TaskPath
+    }
+}
+$results | Sort-Object -Property 'UserId' | Format-Table
